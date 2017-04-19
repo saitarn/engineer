c5a49b942fb2ffcda816f0280f02311a2f9528c7
saitarn <saitarn14@gmail.com>
2017-04-19 17:52:57 +0700

    Create postModel, direct route to db, eloquent

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,5 +8,93 @@ use App\Http\Requests;
 
 class PostsController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($id)
+    {
+        //
+        return "This is messagee from index function". $id;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return "this is create method";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        return "this is the show method , and id is : ". $id;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function contact(){
+        $people = ['jo', 'fon', 'bew', 'kan', 'fun'];
+        //$people = ['jo'];
+        return view('contact', compact('people'));
+    }
+
+    public function show_post($id, $name, $password){
+        // return view('post')->with('id', $id); // sent parameter method1
+        return view('post', compact('id', 'name', 'password'));
+    } 
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,24 +11,87 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/about', function () {
+//     return "This is About Page";
+// });
+
+// Route::get('/contact', function () {
+//     return "This is Contact Page";
+// });
+
+// Route::get('/post/{id}', function($id){
+// 	return "Post page" . $id;
+// });
+
+// Route::get('/admin/posts/example', array('as' => 'admin.home', function(){
+// 	$url = route('admin.home');
+// 	return "this url is". $url;
+// }));
+
+// =============================================
+// Route::get('post/{id}/', 'PostsController@index');
+
+// =============================================
+
+// ================REsource=============================
+Route::resource('posts', 'PostsController');
+Route::get('/contact', 'PostsController@contact');
+Route::get('/post/{id}/{name}/{password}/', 'PostsController@show_post');
+
+// =============================================
+
+// ===========Raw Query Database==================================
+Route::get('/insert', function(){
+	DB::insert('insert into posts(title, content) values(?,?)', ['PHP with laravel2', 'Laravel is the best that has happened to PHP']);
 });
 
-Route::get('/about', function () {
-    return "This is About Page";
+Route::get('/read', function(){
+	$results = DB::select('select * from posts where id = ?', [1]);
+	foreach ($results as $post) {
+		# code...
+		return $post->title;
+	}
 });
 
-Route::get('/contact', function () {
-    return "This is Contact Page";
+Route::get('/update', function(){
+	$updated = DB::update('update posts set title = "Updated title" where id = ?', [1]);
+	return "Updated data ID : ".$updated;
 });
 
-Route::get('/post/{id}', function($id){
-	return "Post page" . $id;
+Route::get('/delete', function(){
+	$deleted = DB::delete('delete from posts where id = ?', [1]);
+	return "Data deleted by id: ". $deleted;
+});
+// =============================================
+
+// =================eloquent=====================
+
+Route::get('read1', function(){
+	//$posts = App\Post;
+	// or "use App\Post" in head
+	$posts = App\Post::all();
+	foreach ($posts as $post) {
+		return $post->title;
+	}
 });
 
-Route::get('/admin/posts/example', array('as' => 'admin.home', function(){
-	$url = route('admin.home');
-	return "this url is". $url;
-}));
+Route::get('find1', function(){
+	$post = App\Post::find(2);
+		return $post;
+});
 
+Route::get('findwhere1', function(){
+	$posts = App\Post::where('id',3)->orderBy('id', 'desc')->take(1)->get();
+	return $posts;
+});
+
+Route::get('findmore1', function(){
+	$posts = App\Post::findOrFail(1);
+	return $posts;
+});
+
+// =================================================
